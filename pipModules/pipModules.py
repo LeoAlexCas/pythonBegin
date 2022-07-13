8bcecc2a41e83e8da5bb713feb17247a4bd1c454
Leonardo <castroleonardo@live.cl>
2022-07-12 20:19:52 -0400

    pip

--- pipModules/pipModules.py
+++ pipModules/pipModules.py
@@ -1,1 +1,5 @@
 #pyp es como npm traera del gestor las librerias que pidamos se usa pip install modulo
+#aca usamos colorama como ejemplo, pero flask y django se consigue desde aca tambien
+import colorama
+
+print(colorama.Fore.RED + 'text')
